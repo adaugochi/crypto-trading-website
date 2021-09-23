f2ab18d5dbf25602ffb69775145c76132ae22090
adaa <adaamgbede@gmail.com>
2021-09-23 19:17:59 +0100

    setup

---
+++ components/Tutorial.vue
@@ -0,0 +1,8 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <h2 class="text-2xl leading-7 font-semibold text-blue">
+      Welcome to your Nuxt Application
+    </h2>
+  </div>
+</template>
