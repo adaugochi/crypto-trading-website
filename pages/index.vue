67c0e134b690356d9fa7739b4eb3e34dd7ed26da
adaa <adaamgbede@gmail.com>
2021-09-27 00:23:03 +0100

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,117 @@
 <template>
-  <Tutorial/>
+    <div>
+        <!-- Header -->
+        <nav class="navbar navbar-light bg-white ">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                  <img src="~/static/logo.png" alt="Logo"  class="d-inline-block align-text-top">
+                </a>
+                <div class="mt-4 mt-sm-0">
+                    <a class="btn btn-dark-outline px-4 px-md-5">Sign in</a>
+                    <a class="btn btn-linear-gradient ms-3">Get started for free</a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Hero Banner -->
+        <section class="container">
+            <h2 class="hero-banner__header-text">
+               The most secured peer to peer crypto-currency platform
+            </h2>
+            <div class="row gy-5 hero-banner__text-wrapper">
+                <div class="col-md col-12">
+                    <h5 class="hero-banner__text-section">Buy with ease</h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="hero-banner__text-section">Sell with ease </h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="hero-banner__text-section">Security at its best</h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-51">
+            <div class="row">
+                <div class="col-12">
+                    <div class="focus-card focus-card__p-lg">
+                        <div class="row gx-5">
+                            <div class="col-md-6">
+                                <p class="focus-card__small text-uppercase">Focus</p>
+                                <h4 class=focus-card__header>
+                                    On capturing those <br> awesome moments
+                                </h4>
+                                <p class="focus-card__text text__success">See how consumers transact</p>
+                                <a class="btn btn-linear-gradient btn__lg">Get Started now</a>
+                            </div>
+                            <div class="col-md-6 mt-5 mt-md-0">
+                                <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-51">
+            <div class="row gx-3">
+                <div class="col-md-5">
+                    <div class="focus-card p-5">
+                        <p class="focus-card__small text-uppercase">Focus</p>
+                        <h4 class=focus-card__header>On capturing those awesome moments</h4>
+                        <p class="focus-card__text text__success">See how consumers transact</p>
+                        <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                    </div>
+                </div>
+                <div class="col-md-7 mt-5 mt-md-0">
+                    <div class="focus-card focus-card__p-md">
+                        <p class="focus-card__small text-uppercase">Focus</p>
+                        <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
+                        <p class="focus-card__text text__success">See how consumers transact</p>
+                        <img src="~/static/design-2.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-51">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="focus-card focus-card__p-md">
+                        <div class="row gx-5">
+                            <div class="col-lg-7">
+                                <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid">
+                            </div>
+                            <div class="col-lg-4 mt-5 mt-lg-0">
+                                <p class="focus-card__small text-uppercase">Focus</p>
+                                <h4 class=focus-card__header>On capturing those awesome moments</h4>
+                                <p class="focus-card__text text__success mb-0">See how consumers transact</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 mt-5 mt-md-0">
+                    <div class="focus-card">
+                        <img src="~/static/design-3.png" alt="design" class="d-inline-block w-web-fill">
+                        <div class="focus-card__p-md">
+                            <p class="focus-card__small text-uppercase">Focus</p>
+                            <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
+                            <p class="focus-card__text text__success mb-0">See how consumers transact</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
