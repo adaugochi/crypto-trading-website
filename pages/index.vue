dcaf4c50ef724d965db5f91fe7d67ab6d734a0b9
adaa <adaamgbede@gmail.com>
2021-10-27 02:12:27 +0100

    landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,228 +1,94 @@
 <template>
-    <div>
-        <!-- Header -->
-        <nav class="navbar navbar-light bg-white ">
-            <div class="container">
-                <a class="navbar-brand" href="/">
-                    <img src="~/static/logo.png" alt="Logo"  class="d-inline-block align-text-top">
-                </a>
-                <div class="mt-4 mt-sm-0">
-                    <a class="btn btn-dark-outline px-4 px-md-5">Sign in</a>
-                    <a class="btn btn-linear-gradient ms-3">Get started for free</a>
-                </div>
-            </div>
-        </nav>
-
-        <!-- Hero Banner -->
-        <section class="container">
-            <h2 class="hero-banner__header-text">
-               The most secured peer to peer crypto-currency platform
-            </h2>
-            <div class="row gy-5 hero-banner__text-wrapper">
-                <div class="col-md col-12">
-                    <h5 class="hero-banner__text-section">Buy with ease</h5>
-                    <p class="text__success hero-banner__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-                <div class="col-md col-12 offset-md-1 offset-lg-1">
-                    <h5 class="hero-banner__text-section">Sell with ease </h5>
-                    <p class="text__success hero-banner__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-                <div class="col-md col-12 offset-md-1 offset-lg-1">
-                    <h5 class="hero-banner__text-section">Security at its best</h5>
-                    <p class="text__success hero-banner__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-            </div>
-        </section>
-
-        <section class="container margin-b-51">
-            <div class="row">
-                <div class="col-12">
-                    <div class="focus-card focus-card__p-lg">
-                        <div class="row gx-md-5">
-                            <div class="col-md-6">
-                                <p class="focus-card__small text-uppercase">Focus</p>
-                                <h4 class=focus-card__header>
-                                    On capturing those <br> awesome moments
-                                </h4>
-                                <p class="focus-card__text text__success">See how consumers transact</p>
-                                <a class="btn btn-linear-gradient btn__lg">Get Started now</a>
-                            </div>
-                            <div class="col-md-6 mt-5 mt-md-0">
-                                <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
-                            </div>
-                        </div>
+    <main class="wrapper">
+        <div class="wrapper-header">
+            <!-- Header -->
+            <nav class="navbar navbar-border-bottom bg-transparent ">
+                <div class="container">
+                    <a class="navbar-brand" href="/">
+                        <img src="~/static/logo.png" alt="Logo" class="d-inline-block align-text-top">
+                    </a>
+                    <div class="mt-4 mt-sm-0">
+                        <a class="btn btn__primary">Sign Up</a>
                     </div>
                 </div>
-            </div>
-        </section>
+            </nav>
 
-        <section class="container margin-b-51">
-            <div class="row gx-md-3">
-                <div class="col-md-5">
-                    <div class="focus-card p-5">
-                        <p class="focus-card__small text-uppercase">Focus</p>
-                        <h4 class=focus-card__header>On capturing those awesome moments</h4>
-                        <p class="focus-card__text text__success">See how consumers transact</p>
-                        <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
-                    </div>
-                </div>
-                <div class="col-md-7 mt-5 mt-md-0">
-                    <div class="focus-card focus-card__p-md">
-                        <p class="focus-card__small text-uppercase">Focus</p>
-                        <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
-                        <p class="focus-card__text text__success">See how consumers transact</p>
-                        <img src="~/static/design-2.png" alt="design" class="d-inline-block img-fluid w-web-fill">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="container margin-b-159">
-            <div class="row">
-                <div class="col-md-7">
-                    <div class="focus-card focus-card__p-md">
-                        <div class="row gx-md-5">
-                            <div class="col-lg-7">
-                                <img src="~/static/design-1.png" alt="design" class="d-inline-block img-fluid">
-                            </div>
-                            <div class="col-lg-4 mt-5 mt-lg-0">
-                                <p class="focus-card__small text-uppercase">Focus</p>
-                                <h4 class=focus-card__header>On capturing those awesome moments</h4>
-                                <p class="focus-card__text text__success mb-0">See how consumers transact</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-5 mt-5 mt-md-0">
-                    <div class="focus-card">
-                        <img src="~/static/design-3.png" alt="design" class="d-inline-block w-web-fill">
-                        <div class="focus-card__p-md">
-                            <p class="focus-card__small text-uppercase">Focus</p>
-                            <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
-                            <p class="focus-card__text text__success mb-0">See how consumers transact</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="bg-brand-light text-center">
-            <div class="container">
+            <!-- hero banner -->
+            <div class="container hero-banner">
                 <div class="row">
-                    <div class="col-md-9 col-lg-7 mx-auto">
-                        <h2 class="section-two__header_text">On capturing those awesome moments </h2>
-                        <p class="focus-card__text text__success mb-5">See how consumers transact</p>
-                        <div class="d-grid gap-2">
-                            <a class="btn btn-linear-gradient btn__lg margin-b-159">Get Started now</a>
-                        </div>
-                        <img src="~/static/design-2.png" alt="design" class="d-inline-block img-fluid w-web-fill margin-b-offset">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <section class="container">
-            <h2 class="section-three__header-text">
-                Let’s shape the future of crypto-currency in Africa
-            </h2>
-            <div class="row gy-5 section-three__text-wrapper">
-                <div class="col-md col-12">
-                    <h5 class="section-three__text-section">Buy with ease</h5>
-                    <p class="text__success section-three__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-                <div class="col-md col-12 offset-md-1 offset-lg-1">
-                    <h5 class="section-three__text-section">Sell with ease </h5>
-                    <p class="text__success section-three__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-                <div class="col-md col-12 offset-md-1 offset-lg-1">
-                    <h5 class="section-three__text-section">Security at its best</h5>
-                    <p class="text__success section-three__sub-text-section">
-                        Kindly verify your profile to start doing more.
-                    </p>
-                </div>
-            </div>
-      </section>
-
-        <section class="bg-white section-four">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-12">
-                        <h2 class="section-four__header-text">
-                            Get updates on industry innovations and the latest updates on our product
-                        </h2>
-                    </div>
-                </div>
-                <div class="mb-3 input-group">
-                    <input type="text" class="form-control form-input" name="email" placeholder="Enter your email here">
-                    <button class="btn btn-linear-gradient btn__lg">Subscribe</button>
-                </div>
-                <div class="row">
-                    <div class="col-lg-7 col-md-10">
-                        By submitting your information, you are agreeing to receive news, surveys, and special offers
-                        from Myddle Cafe Limited. <a href="#" class="text-primary">Terms and Conditions, Privacy Policy</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <!-- Footer -->
-        <section class="footer bg-light">
-            <div class="container">
-                <div class="row footer-first">
-                    <div class="col-lg-3 col-md-2 mb-5 mb-md-0">
-                        <a class="navbar-brand" href="/">
-                            <img src="~/static/footer-logo.png" alt="Logo"  class="d-inline-block align-text-top">
-                        </a>
-                    </div>
-                    <div class="col-md-4 offset-md-2 offset-lg-0 mb-5 mb-md-0">
-                        <p class="mb-4">
-                            Most secured peer-to-peer platform for your crypto-currency transactions
+                    <div class="col-md-6">
+                        <p class="hero-banner__sub-text">With 0% trading commission</p>
+                        <h1 class=hero-banner__header>
+                            Most trusted and secure P2P Crypto- currency exchange
+                        </h1>
+                        <p class="hero-banner__text">
+                            Ushering Africa into the digital age of payment by making
+                            cryptocurrency as easy as possible.
                         </p>
-                        <p>Security as a Lifestyle</p>
+                        <a class="btn btn__primary btn__lg">Start trading</a>
                     </div>
-                    <div class="col-md-4 offset-lg-1">
-                        <p>Connect with us</p>
-                        <div class="footer-socials">
-                            <i class="bi bi-facebook"></i>
-                            <i class="bi bi-twitter icon"></i>
-                            <i class="bi bi-instagram icon"></i>
-                        </div>
-                        <div class="d-flex mt-4">
-                            <i class="bi bi-telephone me-3"></i>
-                            <div>+233596921097, +233544315334, +233246207093</div>
-                        </div>
-                        <div class="d-flex mt-4">
-                            <i class="bi bi-envelope me-3"></i>
-                            <div>info@myddlecafe.com</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-between footer-sub">
-                    <div>
-                        <span class="me-3">© 2021 Myddle Cafe Limited</span>
-                        <span>All rights reserved.</span>
-                    </div>
-                    <div class="text__warning">
-                        <span class="me-3"><a>Terms and Conditions  </a></span>
-                        <span><a>Privacy Policy</a></span>
+                    <div class="col-md-6 mt-5 mt-md-0 align-self-center">
+                        <img src="~/static/new/Group4.png" alt="design" class="d-inline-block img-fluid w-web-fill">
                     </div>
                 </div>
             </div>
-        </section>
-        <!-- End Footer -->
-    </div>
+        </div>
+
+        <!-- why choose us -->
+        <div class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-10 mx-auto text-center">
+                        <h2 class="fs-36 fw-500 pb-4 text__dark">Why choose us</h2>
+                        <p class="why-choose-us__body">
+                            We render the best service on P2P market and here are the reason people choose us
+                        </p>
+                    </div>
+                </div>
+                <div class="row gx-5 mt-5">
+                    <div class="col-md-4" v-for="(offer, index) in offers" :key="index">
+                        <img :src="'./static/new/' + offer.imgPath" alt="why" class="why-choose-us__img">
+                        <h5 class="fw-600 text__primary why-choose-us__title">
+                            {{ offer.title }}
+                        </h5>
+                        <p class="why-choose-us__body">{{ offer.body }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
-export default {}
+export default {
+    name: "index",
+    data() {
+        return {
+            offers: [
+                {
+                    imgPath: 'Group6.png',
+                    title: 'Secure and transparent',
+                    body: `Security is guaranteed. We also maintain privacy as well th quality of our product.
+                        We provide transparent service, our system and eco-system can easily be monitored always`
+                },
+                {
+                    imgPath: 'Group13.png',
+                    title: 'Social interaction',
+                    body: `We have a great platform where traders can interaction together about the coin or
+                    asset they plan on selling or buying as the case may be.`
+                },
+                {
+                    imgPath: 'Group8.png',
+                    title: '24/7 Customer service',
+                    body: `Our customer service is always available to assist you incase of any report or issue you
+                    might be facing at any given time`
+                }
+            ],
+        }
+    }
+}
 </script>
+
+<style lang="scss">
+@import "assets/scss/pages/index";
+</style>
---
+++ pages/oldIndex.vue
@@ -0,0 +1,232 @@
+<template>
+    <div>
+        <!-- Header -->
+        <nav class="navbar navbar-light bg-white ">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <img src="~/static/logo.png" alt="Logo"  class="d-inline-block align-text-top">
+                </a>
+                <div class="mt-4 mt-sm-0">
+                    <a class="btn btn-dark-outline px-4 px-md-5">Sign in</a>
+                    <a class="btn btn-linear-gradient ms-3">Get started for free</a>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Hero Banner -->
+        <section class="container">
+            <h2 class="hero-banner__header-text">
+                The most secured peer to peer crypto-currency platform
+            </h2>
+            <div class="row gy-5 hero-banner__text-wrapper">
+                <div class="col-md col-12">
+                    <h5 class="hero-banner__text-section">Buy with ease</h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="hero-banner__text-section">Sell with ease </h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="hero-banner__text-section">Security at its best</h5>
+                    <p class="text__success hero-banner__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-51">
+            <div class="row">
+                <div class="col-12">
+                    <div class="focus-card focus-card__p-lg">
+                        <div class="row gx-md-5">
+                            <div class="col-md-6">
+                                <p class="focus-card__small text-uppercase">Focus</p>
+                                <h4 class=focus-card__header>
+                                    On capturing those <br> awesome moments
+                                </h4>
+                                <p class="focus-card__text text__success">See how consumers transact</p>
+                                <a class="btn btn-linear-gradient btn__lg">Get Started now</a>
+                            </div>
+                            <div class="col-md-6 mt-5 mt-md-0">
+                                <img src="~/static/old/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-51">
+            <div class="row gx-md-3">
+                <div class="col-md-5">
+                    <div class="focus-card p-5">
+                        <p class="focus-card__small text-uppercase">Focus</p>
+                        <h4 class=focus-card__header>On capturing those awesome moments</h4>
+                        <p class="focus-card__text text__success">See how consumers transact</p>
+                        <img src="~/static/old/design-1.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                    </div>
+                </div>
+                <div class="col-md-7 mt-5 mt-md-0">
+                    <div class="focus-card focus-card__p-md">
+                        <p class="focus-card__small text-uppercase">Focus</p>
+                        <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
+                        <p class="focus-card__text text__success">See how consumers transact</p>
+                        <img src="~/static/old/design-2.png" alt="design" class="d-inline-block img-fluid w-web-fill">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container margin-b-159">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="focus-card focus-card__p-md">
+                        <div class="row gx-md-5">
+                            <div class="col-lg-7">
+                                <img src="~/static/old/design-1.png" alt="design" class="d-inline-block img-fluid">
+                            </div>
+                            <div class="col-lg-4 mt-5 mt-lg-0">
+                                <p class="focus-card__small text-uppercase">Focus</p>
+                                <h4 class=focus-card__header>On capturing those awesome moments</h4>
+                                <p class="focus-card__text text__success mb-0">See how consumers transact</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 mt-5 mt-md-0">
+                    <div class="focus-card">
+                        <img src="~/static/old/design-3.png" alt="design" class="d-inline-block w-web-fill">
+                        <div class="focus-card__p-md">
+                            <p class="focus-card__small text-uppercase">Focus</p>
+                            <h4 class=focus-card__header>On capturing those <br> awesome moments</h4>
+                            <p class="focus-card__text text__success mb-0">See how consumers transact</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-brand-light text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-lg-7 mx-auto">
+                        <h2 class="section-two__header_text">On capturing those awesome moments </h2>
+                        <p class="focus-card__text text__success mb-5">See how consumers transact</p>
+                        <div class="d-grid gap-2">
+                            <a class="btn btn-linear-gradient btn__lg margin-b-159">Get Started now</a>
+                        </div>
+                        <img src="~/static/old/design-2.png" alt="design" class="d-inline-block img-fluid w-web-fill margin-b-offset">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container">
+            <h2 class="section-three__header-text">
+                Let’s shape the future of crypto-currency in Africa
+            </h2>
+            <div class="row gy-5 section-three__text-wrapper">
+                <div class="col-md col-12">
+                    <h5 class="section-three__text-section">Buy with ease</h5>
+                    <p class="text__success section-three__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="section-three__text-section">Sell with ease </h5>
+                    <p class="text__success section-three__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+                <div class="col-md col-12 offset-md-1 offset-lg-1">
+                    <h5 class="section-three__text-section">Security at its best</h5>
+                    <p class="text__success section-three__sub-text-section">
+                        Kindly verify your profile to start doing more.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-white section-four">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-md-12">
+                        <h2 class="section-four__header-text">
+                            Get updates on industry innovations and the latest updates on our product
+                        </h2>
+                    </div>
+                </div>
+                <div class="mb-3 input-group">
+                    <input type="text" class="form-control form-input" name="email" placeholder="Enter your email here">
+                    <button class="btn btn-linear-gradient btn__lg">Subscribe</button>
+                </div>
+                <div class="row">
+                    <div class="col-lg-7 col-md-10">
+                        By submitting your information, you are agreeing to receive news, surveys, and special offers
+                        from Myddle Cafe Limited. <a href="#" class="text-primary">Terms and Conditions, Privacy Policy</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <section class="footer bg-light">
+            <div class="container">
+                <div class="row footer-first">
+                    <div class="col-lg-3 col-md-2 mb-5 mb-md-0">
+                        <a class="navbar-brand" href="/">
+                            <img src="~/static/old/footer-logo.png" alt="Logo"  class="d-inline-block align-text-top">
+                        </a>
+                    </div>
+                    <div class="col-md-4 offset-md-2 offset-lg-0 mb-5 mb-md-0">
+                        <p class="mb-4">
+                            Most secured peer-to-peer platform for your crypto-currency transactions
+                        </p>
+                        <p>Security as a Lifestyle</p>
+                    </div>
+                    <div class="col-md-4 offset-lg-1">
+                        <p>Connect with us</p>
+                        <div class="footer-socials">
+                            <i class="bi bi-facebook"></i>
+                            <i class="bi bi-twitter icon"></i>
+                            <i class="bi bi-instagram icon"></i>
+                        </div>
+                        <div class="d-flex mt-4">
+                            <i class="bi bi-telephone me-3"></i>
+                            <div>+233596921097, +233544315334, +233246207093</div>
+                        </div>
+                        <div class="d-flex mt-4">
+                            <i class="bi bi-envelope me-3"></i>
+                            <div>info@myddlecafe.com</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-between footer-sub">
+                    <div>
+                        <span class="me-3">© 2021 Myddle Cafe Limited</span>
+                        <span>All rights reserved.</span>
+                    </div>
+                    <div class="text__warning">
+                        <span class="me-3"><a>Terms and Conditions  </a></span>
+                        <span><a>Privacy Policy</a></span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Footer -->
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import "assets/scss/pages/old-index";
+</style>
