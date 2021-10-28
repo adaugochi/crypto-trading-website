4a1c81c008872f0e7f7e51ab7da78cde8c185c2b
adaa <adaamgbede@gmail.com>
2021-10-28 02:07:12 +0100

    landing page

---
+++ components/InputField.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class='form-input-wrapper'>
+        <input :type="type" class="form-input" :placeholder="placeholder" v-model="value">
+        <span class="form-input-placeholder">{{ placeholder }}</span>
+        <div class="mt-1">
+            <small class="form-hint text-danger">{{ errorMsg }}</small>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InputField",
+    props: {
+        type: {
+            type: String,
+            default: "text"
+        },
+        value: {
+            type: String
+        },
+        placeholder: {
+            type: String,
+            default: ""
+        },
+        errorMsg: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
                     <a class="navbar-brand" href="/">
                         <img src="~/static/logo.png" alt="Logo" class="d-inline-block align-text-top">
                     </a>
-                    <div class="mt-4 mt-sm-0">
+                    <div>
                         <a class="btn btn__primary">Sign Up</a>
                     </div>
                 </div>
@@ -38,20 +38,121 @@
         <div class="py-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6 col-md-10 mx-auto text-center">
+                    <div class="col-lg-8 col-xl-6 col-md-10 mx-auto text-center">
                         <h2 class="fs-36 fw-500 pb-4 text__dark">Why choose us</h2>
-                        <p class="why-choose-us__body">
+                        <p class="font-normal">
                             We render the best service on P2P market and here are the reason people choose us
                         </p>
                     </div>
                 </div>
-                <div class="row gx-5 mt-5">
+                <div class="row gx-2 mt-5">
                     <div class="col-md-4" v-for="(offer, index) in offers" :key="index">
-                        <img :src="'./static/new/' + offer.imgPath" alt="why" class="why-choose-us__img">
+                        <img :src="require('~/static/new/' + offer.imgPath)" alt="why" class="why-choose-us__img">
                         <h5 class="fw-600 text__primary why-choose-us__title">
                             {{ offer.title }}
                         </h5>
-                        <p class="why-choose-us__body">{{ offer.body }}</p>
+                        <p class="font-normal">{{ offer.body }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Our Product -->
+        <div class="tabs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active text-center" id="buy-coin-tab" data-bs-toggle="tab" data-bs-target="#buyCoin"
+                                        type="button" role="tab" aria-controls="buy-coin" aria-selected="true">
+                                    <img src="~/static/new/buy-active.svg" alt="buy" height="24"><br>
+                                    <span class="text__dark mt-1">Buy coin</span>
+                                </a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="sell-coin-tab" data-bs-toggle="tab" data-bs-target="#sellCoin"
+                                        type="button" role="tab" aria-controls="sell-coin" aria-selected="false">
+                                    <img src="~/static/new/sell-inactive.svg" alt="sell" height="24"><br>
+                                    <span class="text__dark mt-1">Sell coin</span>
+                                </button>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="buyCoin" role="tabpanel" aria-labelledby="buy-coin-tab">
+                                <input-field
+                                    placeholder="You buy"
+                                    value="1,000"
+                                />
+                                <input-field
+                                    placeholder="You get"
+                                    value="0.0027"
+                                />
+                            </div>
+                            <div class="tab-pane fade" id="sellCoin" role="tabpanel" aria-labelledby="sell-coin-tab">
+                                <input-field
+                                    placeholder="You buy"
+                                    value="0.0027"
+                                />
+                                <input-field
+                                    placeholder="You get"
+                                    value="900"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="offset-md-2 col-md-4">
+                        <h2 class="fs-24 fw-500 pb-4 text__dark">Our Product</h2>
+                        <p class="font-normal">
+                            Myddle Cafe is a decentralized financial platform where buyers and sellers are can trade
+                            cryptocurrency. Ushering Africa into the digital age of payment by making
+                            cryptocurrency as easy as possible.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Other Services -->
+        <div class="pdb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-6 col-md-10 mx-auto text-center">
+                        <h2 class="fs-36 fw-500 pb-4 text__dark">Other Services</h2>
+                        <p class="font-normal">
+                            We also render variety of service that can help you transact in a DeFi as effective
+                            as that of your bank activities
+                        </p>
+                    </div>
+                </div>
+                <div class="bg-bar-img">
+                    <div class="row gx-2 mt-5">
+                        <div class="col-md-4" v-for="(service, index) in services" :key="index">
+                            <img :src="require('~/static/new/' + service.imgPath)" alt="why" class="why-choose-us__img">
+                            <h5 class="fw-600 text__primary why-choose-us__title">
+                                {{ service.title }}
+                            </h5>
+                            <p class="font-normal">{{ service.body }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Explore -->
+        <div class="bg-explore-img">
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-md-10 mx-auto text-white text-center">
+                        <div class="card bg__primary">
+                            <h2 class="fw-500 fs-36">
+                                Get ready to explore the Crypto world
+                            </h2>
+                            <p>
+                                We have a great platform where traders can interaction together about the coin or
+                                asset they plan on selling or buying as the case may be. <a class="fw-500">Sign up now</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,28 +161,48 @@
 </template>
 
 <script>
+import InputField from "../components/InputField";
 export default {
     name: "index",
+    components: {InputField},
     data() {
         return {
             offers: [
                 {
-                    imgPath: 'Group6.png',
+                    imgPath: 'Group6.svg',
                     title: 'Secure and transparent',
                     body: `Security is guaranteed. We also maintain privacy as well th quality of our product.
                         We provide transparent service, our system and eco-system can easily be monitored always`
                 },
                 {
-                    imgPath: 'Group13.png',
+                    imgPath: 'Group13.svg',
                     title: 'Social interaction',
                     body: `We have a great platform where traders can interaction together about the coin or
                     asset they plan on selling or buying as the case may be.`
                 },
                 {
-                    imgPath: 'Group8.png',
+                    imgPath: 'Group8.svg',
                     title: '24/7 Customer service',
                     body: `Our customer service is always available to assist you incase of any report or issue you
                     might be facing at any given time`
+                }
+            ],
+            services: [
+                {
+                    imgPath: 'Group9.svg',
+                    title: 'Buy Airtime',
+                    body: `You can now buy airtime using our crypto currency platform with ease and comfort`
+                },
+                {
+                    imgPath: 'Group10.svg',
+                    title: 'Pay Bills',
+                    body: `With our platform, paying bill with your crypto currency has been made possible with
+                    ease and without withdrawing as well.`
+                },
+                {
+                    imgPath: 'Group11.svg',
+                    title: 'Swap Coins',
+                    body: `We also offer the option to swap coins with little or no charge involved with the click of a button.`
                 }
             ],
         }
