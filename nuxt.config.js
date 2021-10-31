b90d7c8fb0a4d52edd50af92b58507b36b2fb2a6
adaa <adaamgbede@gmail.com>
2021-10-31 22:57:12 +0000

    landing page

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  mode: 'universal',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'myddle-cafe-website',
